6ecfec7f5de20d33b7f84ae2fd9a840c68b76c08
myan <mryanhaoxd@163.com>
2017-08-02 21:02:00 +0800

    section2-3 change

--- task/task3-pre-pos/main/section-2/practice-3.js
+++ task/task3-pre-pos/main/section-2/practice-3.js
@@ -1,40 +1,61 @@
 'use strict';
 
 module.exports = function countSameElements(collection) {
-      var arr_SameElements = [];
-      var counts = {};
-      var specCounts = {};
-      for(var i=0;i<collection.length; i++){
-            var num = collection[i];
-           if(num.indexOf('-') != -1) {
-                 var kvPair = collection[i].split('-');
-                 if(kvPair.length === 2){
-                        specCounts[kvPair[0]] = parseInt(kvPair[1]);
-                 }
-           }else if(num.indexOf(':') != -1){
-                 var kvPair = collection[i].split(':');
-                 if(kvPair.length === 2){
-                        specCounts[kvPair[0]] = kvPair[1];
-                 }
-           }else if(num.indexOf('[') != -1){
-                 var k = num.substring(0,1)
-                 var v = parseInt(num.substring(num.indexOf('[')+1,num.indexOf(']')));
-                 specCounts[k]=v;
-           }else{
-                  counts[num] = counts[num] ? counts[num] + 1 :1;
-           }
-      }
-      var keyArray = Object.keys(counts);
-      var specArray = Object.keys(specCounts);
-      for(var i = 0; i<keyArray.length;i++){
-            var count = counts[keyArray[i]];
-            for(var j =0;j<specArray.length;j++){
-                  if(keyArray[i] === specArray[j]){
-                       count +=  specCounts[specArray[j]];
-                  }
+    var result = [];
+    for (var i = 0; i < collection.length; i++) {
+        var num = collection[i];
+        if (num.indexOf('-') !== -1) {
+            var kvPair = collection[i].split('-');
+            result.push({
+                name: kvPair[0],
+                summary:parseInt(kvPair[1])
+            });
+        } else if (num.indexOf(':') !== -1) {
+            var kvPair = collection[i].split(':');
+            result.push({
+                name: kvPair[0],
+                summary: parseInt(kvPair[1])
+            });
+        } else if (num.indexOf('[') != -1) {
+            //var splitNum = num.split('');
+            //var summary = parseInt(num[2] + num[3]);
+
+            //var name = num[0];
+            result.push({
+                name: num[0],
+                summary: parseInt(num.substring(num.indexOf('[')+1,num.indexOf(']')))
+            });
+        } else {
+            //findInResult 需要自己实现,定义一个函数，查找collection的字母是否已经被收集
+            if (!findInResult(result, num)) {
+                result.push({name: num, summary: 1});
+            } else {
+                var k = result.find(function(r){
+                    return r.name === num;
+                });
+                k.summary++;
             }
-            arr_SameElements.push({name:keyArray[i],summary:count})
-      }
-      return arr_SameElements;
-      
+        }
+    }
+    //let's build together
+    var finalResult = [];
+    for (var i = 0; i < result.length; i++) {
+        var k = result[i].name;
+        var v = result[i].summary;
+        if (findInResult(finalResult, k)) {
+            var tobeAdded = finalResult.find(r => k === r.name);
+            tobeAdded.summary += parseInt(v);
+        } else {
+            finalResult.push({name: k, summary: v});
+        }
+    }
+    return finalResult;
+}
+
+function findInResult(result, num) {
+    for (var i = 0; i < result.length; i++) {
+            if (result[i].name ===  num) {
+            return true;
+        }
+    }
 }
